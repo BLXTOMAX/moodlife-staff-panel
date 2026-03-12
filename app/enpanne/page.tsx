b25a595f9893784944fc535d757e70ff4b259be9
BLXTOMAX <paristom356@gmail.com>
2026-03-13 00:20:58 +0100

    update

---
+++ app/enpanne/page.tsx
@@ -0,0 +1,11 @@
+export default function EnPannePage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black">
+      <img
+        src="/enpanne.png"
+        alt="En panne"
+        className="max-w-full max-h-screen object-contain"
+      />
+    </div>
+  );
+}
